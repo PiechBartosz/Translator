63f819006fcaf9510df4f7c5ea888868b711e620
PiechBartosz <p.bartosz@yahoo.com>
2023-09-22 14:55:12 +0200

    button and mobile navigation

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 const burgerBtn = document.querySelector(".burger-btn");
 const mobileMenu = document.querySelector(".navigation__mobile");
 const navigation = document.querySelector(".navigation");
-
-// const showMobile = () => {
-// 	mobileMenu.classList.toggle("navigation__mobile--show");
-// 	// navigation.style.backgroundColor = "transparent";
-// 	if (mobileMenu.classList.contains("navigation__mobile--show")) {
-// 		navigation.style.backgroundColor = "transparent";
-// 	} else {
-// 		navigation.style.backgroundColor = "";
-// 	}
-// };
-
-// burgerBtn.addEventListener("click", showMobile);
+const navlinks = document.querySelectorAll(".navigation__mobile-link");
 
 const showMobile = () => {
 	if (mobileMenu.classList.contains("navigation__mobile--show")) {
@@ -20,8 +9,12 @@ const showMobile = () => {
 	} else {
 		navigation.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
 	}
-
 	mobileMenu.classList.toggle("navigation__mobile--show");
+	delayLinks();
+};
+
+const delayLinks = () => {
+	navlinks.forEach((item) => item.classList.toggle("linksAnimation"));
 };
 
 burgerBtn.addEventListener("click", showMobile);
